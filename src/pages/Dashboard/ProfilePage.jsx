9ec26e92c8b615903b8342fb89020ed31e5b6aec
Julian Camilo Cruz <julian20141016@gmail.com>
2021-03-29 20:24:49 -0500

    Finalizado diseño de pagina de Mi Perfil

--- src/pages/Dashboard/ProfilePage.jsx
+++ src/pages/Dashboard/ProfilePage.jsx
@@ -9,11 +9,13 @@ import {
   Avatar,
   Typography,
   Row,
-  Col
+  Col,
+  DemoBox,
 } from "antd";
 import { SmileTwoTone } from "@ant-design/icons";
 
 import eanLogo from "../../assets/ean.jpg";
+import bannerEANX from "../../assets/EANX.png";
 
 const { Title, Text, Paragraph, Link } = Typography;
 const { Header, Content, Sider } = Layout;
@@ -35,72 +37,79 @@ const ProfilePage = ({ user }) => {
             minHeight: 280,
           }}
         >
-          <Row>
-            <Col><Card hoverable className="cardSpaceProfile">
-              <center>
-                <Space direction="horizontal">
-                  <img src={eanLogo} alt="" style={{ maxWidth: "120px" }} />
-                  <Paragraph>
-                    <Title level={2}>Ingenieria Ambiental</Title>
-                    <Title level={5}>Semestre 4</Title>
-                    87 creditos aprobados
-                  </Paragraph>
-                </Space>
-              </center>
-              <Progress
-                strokeColor={{
-                  "0%": "#108ee9",
-                  "100%": "#87d068",
-                }}
-                percent={86}
-              />
-            </Card></Col>
-            <Col>
+          <Row justify="space-around" align="middle" gutter={[32, 32]}>
+            <Col sm={24} md={12} xxl={8}>
+              <Card hoverable className="cardSpaceProfile">
+                <center>
+                  <Space direction="horizontal">
+                    <img src={eanLogo} alt="" style={{ maxWidth: "120px" }} />
+                    <Paragraph>
+                      <Title level={2}>Ingenieria Ambiental</Title>
+                      <Title level={5}>Semestre 4</Title>
+                      87 creditos aprobados
+                    </Paragraph>
+                  </Space>
+                </center>
+                <Progress
+                  strokeColor={{
+                    "0%": "#108ee9",
+                    "100%": "#87d068",
+                  }}
+                  percent={86}
+                />
+              </Card>
+            </Col>
+            <Col sm={24} md={12} xl={8}>
               {user && (
                 <div className="cardSpaceProfile">
-
                   <Card hoverable>
-                    <Space>
-                      <div style={{ padding: "15px" }}>
-                        <Badge count={5} className="avatarImage">
-                          <Avatar
-                            size={64}
-                            src={<SmileTwoTone />}
-                            style={{ fontSize: "65px" }}
-                          />
-                        </Badge>
-                      </div>
-                      <Typography>
-                        <Title level={3}>
-                          {user.name !== "" ? user.name : "Nombre Apellido"}
-                        </Title>
-                        <Text code>
-                          {user.user !== ""
-                            ? "@" + user.user
-                            : "@nombreDeUsuario"}
-                        </Text>
-                        <Paragraph>
-                          <Text type="secondary">
-                            {user.corporate !== "" ? user.corporate : "Entidad"}
+                    <center>
+                      <Space>
+                        <div style={{ padding: "15px" }}>
+                          <Badge count={5} className="avatarImage">
+                            <Avatar
+                              size={64}
+                              src={<SmileTwoTone />}
+                              style={{ fontSize: "65px" }}
+                            />
+                          </Badge>
+                        </div>
+                        <Typography>
+                          <Title level={3}>
+                            {user.name !== "" ? user.name : "Nombre Apellido"}
+                          </Title>
+                          <Text code>
+                            {user.user !== ""
+                              ? "@" + user.user
+                              : "@nombreDeUsuario"}
                           </Text>
-                          <br />
-                          <Text strong>
-                            {user.mail !== "" ? user.mail : "Correo electrónico"}
-                          </Text>
-                        </Paragraph>
-                      </Typography>
-                    </Space>
+                          <Paragraph>
+                            <Text type="secondary">
+                              {user.corporate !== ""
+                                ? user.corporate
+                                : "Entidad"}
+                            </Text>
+                            <br />
+                            <Text strong>
+                              {user.mail !== ""
+                                ? user.mail
+                                : "Correo electrónico"}
+                            </Text>
+                          </Paragraph>
+                        </Typography>
+                      </Space>
+                    </center>
                     <hr />
                     <center>
                       <Link href="https://ant.design" target="_blank">
                         Editar mis datos personales
-                    </Link>
+                      </Link>
                     </center>
                   </Card>
                 </div>
               )}
             </Col>
-            <Col>
+            <Col sm={24} md={12} xl={8}>
               <Card hoverable className="cardSpaceProfile center">
                 <center>
                   <Progress
@@ -123,11 +132,23 @@ const ProfilePage = ({ user }) => {
 
                   <Link href="https://ant.design" target="_blank">
                     Registrar más cursos
-                </Link>
+                  </Link>
                 </center>
               </Card>
             </Col>
           </Row>
+          <center>
+            <div className="bannerContainer">
+              <img
+                src={bannerEANX}
+                alt=""
+                className="bannerEAN"
+                onClick={() => {
+                  window.location = "https://www.eanx.io/";
+                }}
+              />
+            </div>
+          </center>
         </Content>
       </div>
     </Fragment>
